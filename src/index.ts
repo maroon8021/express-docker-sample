4281c87323882ba368dbabe913d195605035319d
Mitsui <maroon8021@gmail.com>
2020-09-11 13:14:05 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,8 @@
 import * as express from "express"
-import * as pg from "pg"
 
 const app = express()
 const port = 3000
 
 app.get("/", (req, res) => res.send("Hello World!"))
-
-app.get("/getSample", (req, res) => {
-  // const pool = new pg.Pool({
-  //   database: "sample",
-  //   user: "root",
-  //   password: "root",
-  //   host: process.env.HOST,
-  //   port: 55000,
-  // })
-  const pool = new pg.Pool({
-    connectionString: "postgresql://root:root@127.0.0.1:55000/sample",
-  })
-  console.log(pool)
-
-  pool.connect((err, client) => {
-    if (err) {
-      console.log(err)
-    }
-    client.query("SELECT * FROM sample", (error, result) => {
-      res.send(JSON.stringify(result))
-    })
-  })
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
